bb015812b97c3e2003f66ae7b28b1203a5da223a
BugHunter-X <askzenai@gmail.com>
2024-12-17 05:40:05 +0100

    Add bugSolution.hh file

---
+++ bugSolution.hh
@@ -0,0 +1,12 @@
+function foo(x: int): int {
+  var result: int = 1;
+  for (var i = 1; i <= x; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+function main(): void {
+  echo foo(5);
+}
+This iterative version avoids stack overflow by not making recursive function calls.
